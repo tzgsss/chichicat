3062519024b896dbaf5eba62d5f8744afadcbc6e
tzgsss <167365689+tzgsss@users.noreply.github.com>
2024-04-20 18:23:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById('token-address').addEventListener('click', function() {
     const tokenAddress = this.innerText;
     navigator.clipboard.writeText(tokenAddress).then(() => {
-        alert('Token address copied to clipboard!');
+        alert('Email address copied to clipboard!');
     }).catch(err => {
         console.error('Error in copying text: ', err);
     });
